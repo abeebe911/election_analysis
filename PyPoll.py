f15fc92f8de3b9f4618a3abdca1429d92b0b2cec
abeebe911 <72118973+abeebe911@users.noreply.github.com>
2020-10-17 22:41:56 -0500

    Add files via upload

--- PyPoll.py
+++ PyPoll.py
@@ -1,3 +1,20 @@
+<<<<<<< HEAD
+# Add our dependencies.
+import csv
+import os
+# Assign a variable to load a file from a path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+    file_reader = csv.reader(election_data)
+
+    # Read and print the header row.
+    headers = next(file_reader)
+    print(headers)
+=======
 # The data we need to retrieve.
 # 1. The total number of votes  cast
 # 2. A complete list of candidates who received votes
@@ -26,3 +43,4 @@ with open(file_to_save, "w") as txt_file:
 
  # Write three counties to the file.
     txt_file.write("Arapahoe\nDenver\nJefferson")
+>>>>>>> ba02c3819d761d4482841c94696f498b5daf51ef
